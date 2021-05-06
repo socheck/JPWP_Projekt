47e531e4fa55c639351dd2fc5b7a6982d91978f2
soczynski <jsoczynski@student.agh.edu.pl>
2021-05-06 21:51:12 +0200

    Dodana rejestracja, logowanie, wylogowywanie

--- wypozyczalnia_app/urls.py
+++ wypozyczalnia_app/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
     path('cena/', views.cena),
     path('flota/', views.flota),
     path('kontakt/', views.kontakt),
     path('rejestracja/', views.rejestracja),
+    path('test/', views.test),
+    path('logowanie/', views.logowanie, name="logowanie"),
+    path('wylogowywanie/', views.wylogowywanie, name="wylogowywanie"),
 ]
