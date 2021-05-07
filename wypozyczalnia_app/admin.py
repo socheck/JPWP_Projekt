fb39081342bfd33b37ce25bad6922fde1aaf82b4
gasz <1pawsao1@gmail.com>
2021-05-07 18:40:49 +0200

    miej nas w opiece (bez migracji) [bez folderów w static]

--- wypozyczalnia_app/admin.py
+++ wypozyczalnia_app/admin.py
@@ -5,6 +5,11 @@ from django.db import models
 from django_json_widget.widgets import JSONEditorWidget
 from .models import Miasto
 from .models import Strefa
+from .models import Oddzial
+from .models import TypAuta
+from .models import Samochod
+from .models import Hulajnoga
+from .models import Profil
 
 
 
@@ -21,3 +26,10 @@ class YourModelAdmin1(admin.ModelAdmin):
         # fields.JSONField: {'widget': JSONEditorWidget}, # if django < 3.1
         models.JSONField: {'widget': JSONEditorWidget},
     }
+
+admin.site.register(Oddzial)
+admin.site.register(TypAuta)
+admin.site.register(Samochod)
+admin.site.register(Hulajnoga)
+
+admin.site.register(Profil)
