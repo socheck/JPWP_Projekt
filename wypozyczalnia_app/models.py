93b05174fa047e18a2b33d066e3829b701a07cdb
gasz <1pawsao1@gmail.com>
2021-05-08 17:11:38 +0200

    migracje

--- wypozyczalnia_app/models.py
+++ wypozyczalnia_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from uuid import uuid4
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+import os
 import datetime
 # Create your models here.
 
@@ -191,4 +191,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profil.save()
+    except:
+        pass
