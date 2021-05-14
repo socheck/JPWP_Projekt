b2496298d9b383a9057385801f50eb9633d3f5e8
soczynski <jsoczynski@student.agh.edu.pl>
2021-05-15 00:04:22 +0200

    QR + wstęp do sprawdzania zgodności danych w bazie

--- wypozyczalnia_app/views.py
+++ wypozyczalnia_app/views.py
@@ -213,6 +213,11 @@ def car_type_selection(request, car_type):
 
 def krotkoterminowy_wynajety(request, car_type, auto_id):
     auto = get_object_or_404(Samochod, id=auto_id)
+    if request.method == 'POST':
+        if(request.POST['kod_samochod'] == 1 ): # tutaj dodać pole kod
+            return render(request, 'koszt.html')
+        else:
+            messages.info(request, 'Wprowadzono niepoprawny kod! Spróbuj ponownie')
     
     miasta = Miasto.objects.all()
     return render(request, 'krotkoterminowy_wynajety.html', {'miasta': miasta,'czy_super' : 'jest super', })
