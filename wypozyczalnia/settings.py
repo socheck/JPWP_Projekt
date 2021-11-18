38a811d9e3149a58ee8923c6ede415ba51e1aa5d
socheck <56121251+socheck@users.noreply.github.com>
2021-11-18 14:10:49 +0100

    Update settings.py

--- wypozyczalnia/settings.py
+++ wypozyczalnia/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vpa39su7jni#(po3$ugdj+yyrb5)=z93bu%=%)13@)(&ybn923'
+SECRET_KEY = 'django-insecure-vpa39*****************923'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'wypozyczalniajpwp@gmail.com'
-EMAIL_HOST_PASSWORD = 'Wypozyczalnia1!'
+EMAIL_HOST_USER = 'wypozycz***********@gmail.com'
+EMAIL_HOST_PASSWORD = '**********************'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
